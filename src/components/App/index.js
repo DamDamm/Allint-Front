cb1eb723c95f53e9bb0be843cad942410988bbe5
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-02-21 14:09:36 +0100

    add repo Search Header footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,19 +4,20 @@ import './styles.css';
 
 //Components
 import Home from 'src/components/Home';
+import Header from '../Header';
 import Product from 'src/components/Product';
 import Profil from 'src/components/Profil';
 import Results from 'src/components/Results';
+import Footer from '../Footer';
 import MentionsLegales from '../MentionsLegales';
 import Error from '../Error';
 
 
-// == Composant
+// == App
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Composant : App</h1>
+      
     </div>
   );
 }
