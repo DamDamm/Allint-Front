dd3512d462983147b3dd20a2784a34027c04b8fd
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-02-22 22:42:36 +0100

    :recycle: Refacto (Login and Password) Simple Form

--- src/components/LoginForm/Login/index.js
+++ src/components/LoginForm/Login/index.js
@@ -1,46 +1,11 @@
-import usersData from 'src/data/MOCK_users.json'
-
-import PropTypes from 'prop-types';
-import { useState } from 'react'
-
-    const Login = (
-        isLogged
-        ) => {
-    const [login, setlogin] = useState('');
-
-    const handleChange = (event) => {
-        setlogin(event.target.value);
-        }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if(isEmail(login)){
-            console.log('email ok')
-        }else {
-            console.log('erreur')
-        }
-    }
-
+const Login = () => {
     return (
         <div>
-            {isLogged ? ( 
-            <form className='form' onSubmit={handleSubmit}>
-                <label>Entrez votre Email</label>
-                <input 
-                type ="email" 
-                name ="email" 
-                placeholder ="Adresse Email" 
-                value ={login} 
-                onChange ={handleChange} /> 
-            </form>
-            ) : (console.log('Veuillez entrer un mail'))}  
+            <form>
+                <input type="text" name="Login" id="Login" />          
+            </form>     
         </div>
     )
 };
-
-Login.propTypes = {
-    isLogged: PropTypes.bool
-}
-
 
 export default Login;
