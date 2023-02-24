033d678c59fcbd9a771be1d38a441d3549328abb
MathildeKoucem <koucem.g@gmail.com>
2023-02-24 11:50:07 +0100

    :ambulance: fix(Search) find product in JSON array

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -5,22 +5,21 @@ import BrandInput from "./BrandInput";
 import ProductInput from "./ProductInput";
 import CategoryInput from "./CategoryInput";
 import productData from "../../data/MOCK_products.json"
-import { element } from 'prop-types';
+import { element, elementType } from 'prop-types';
 
 const Search = () => {
   const [product, setProduct] = useState('');
   
-  const findProduct = () => {console.log(productData.find(el => element.value==product.value))};
+  
 
   const handleChange = (event) => {
-    setProduct(event.target.value)
+    setProduct(event.target.value) 
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log({product})
-    findProduct();
-    console.log(element)
+    console.log(productData.find(el => el.name === product))
   };
 
     return (
