52f82e8ebafe9acb7c841b55525f2407b7acdf69
LordThi <92553090+LordThi@users.noreply.github.com>
2023-02-24 17:32:44 +0100

    :twisted_rightwards_arrows: build => merge preparation to dev branch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,112 +1,30 @@
 // == Import
+import reactLogo from './react-logo.svg';
 import './styles.css';
-import { useState } from 'react';
 
 // Components
-// import Home from 'src/components/Home';
-// import Product from 'src/components/Product';
-// import Search from 'src/components/Search';
-// import Results from 'src/components/Results';
-// import Header from '../Header';
-// import Footer from '../Footer';
-// import Error from '../Error';
-import ProfileForm from '../ProfileForm';
+import Home from 'src/components/Home';
+import Product from 'src/components/Product';
+import Search from 'src/components/Search';
+import Results from 'src/components/Results';
+import Header from '../Header';
+import Footer from '../Footer';
+import Error from '../Error';
+import LoginForm from '../LoginForm';
 
 // == App
-const App = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [adress, setAdress] = useState('');
-  const [zipcode, setZipcode] = useState('');
-  const [city, setCity] = useState('');
-  const [allergyChoice, setAllergyChoice] = useState([]);
+const App = () => (
+  <div className="app">
 
-  // controlled inputs control
-  // console.log(`email = ${email}`);
-  // console.log(`password = ${password}`)
-  // console.log(`name = ${name}`)
-  // console.log(`lastname = ${lastname}`)
-  // console.log(`adress = ${adress}`)
-  // console.log(`zipcode = ${zipcode}`)
-  // console.log(`city = ${city}`)
-  // console.log(`allergies: ${allergyChoice}`);
+    <Header />
+    <Search />
+    <Results />
+    <Product />
+    <Footer />
+    <LoginForm />
 
-  const handleEmailChangeField = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChangeField = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleNameChangeField = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleLastnameChangeField = (event) => {
-    setLastname(event.target.value);
-  };
-
-  const handleAdressChangeField = (event) => {
-    setAdress(event.target.value);
-  };
-
-  const handleZipcodeChangeField = (event) => {
-    setZipcode(event.target.value);
-  };
-
-  const handleCityChangeField = (event) => {
-    setCity(event.target.value);
-  };
-
-  const handleCheckChangeOnClick = (event) => {
-    const allergyName = event.target.name;
-    const isChecked = event.target.checked;
-
-    if (isChecked) {
-      setAllergyChoice([...allergyChoice, allergyName]);
-    }
-    else {
-      setAllergyChoice(allergyChoice.filter((allergy) => allergy !== allergyName));
-    }
-  };
-
-  const handleSubmitClick = (event) => {
-    event.preventDefault();
-    // Fonction d'envoi des données au BACKEND (POST ou UPDATE)
-  };
-
-  return (
-    <div className="app">
-      <ProfileForm
-        email={email}
-        password={password}
-        name={name}
-        lastname={lastname}
-        adress={adress}
-        zipcode={zipcode}
-        city={city}
-        emailChangeField={handleEmailChangeField}
-        passwordChangeField={handlePasswordChangeField}
-        nameChangeField={handleNameChangeField}
-        lastnameChangeField={handleLastnameChangeField}
-        adressChangeField={handleAdressChangeField}
-        zipcodeChangeField={handleZipcodeChangeField}
-        cityChangeField={handleCityChangeField}
-        checkChangeOnClick={handleCheckChangeOnClick}
-        handleClick={handleSubmitClick}
-      />
-      {/* <Header />
-      <Search />
-      <Results />
-      <Product />
-      <Footer /> */}
-
-    </div>
-  );
-};
+  </div>
+);
 
 // == Export
 export default App;
--- src/components/Profil/index.js
+++ src/components/Profil/index.js
@@ -1,25 +1,92 @@
-import AllergyAdd from "./AllergyAdd";
-import AllergyChoice from "./AllergyChoice";
-import FirstName from "./FirstName";
-import LastName from "./LastName";
-import Login from "./Login";
-import Password from "./Password";
+import './styles.scss';
+import { useState } from 'react';
+import ProfileForm from '../ProfileForm';
 
 const Profil = () => {
-    return (
-        <div>
-            <img />
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [adress, setAdress] = useState('');
+  const [zipcode, setZipcode] = useState('');
+  const [city, setCity] = useState('');
+  const [allergyChoice, setAllergyChoice] = useState([]);
 
-            <Login />
-            <Password />
-            <LastName />
-            <FirstName />
-            <AllergyChoice />
-            <AllergyAdd />
+  // controlled inputs control
+  // console.log(`email = ${email}`);
+  // console.log(`password = ${password}`)
+  // console.log(`name = ${name}`)
+  // console.log(`lastname = ${lastname}`)
+  // console.log(`adress = ${adress}`)
+  // console.log(`zipcode = ${zipcode}`)
+  // console.log(`city = ${city}`)
+  // console.log(`allergies: ${allergyChoice}`);
 
-            <button> Enregistrer </button>
-        </div>
-    )
+  const handleEmailChangeField = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChangeField = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleNameChangeField = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleLastnameChangeField = (event) => {
+    setLastname(event.target.value);
+  };
+
+  const handleAdressChangeField = (event) => {
+    setAdress(event.target.value);
+  };
+
+  const handleZipcodeChangeField = (event) => {
+    setZipcode(event.target.value);
+  };
+
+  const handleCityChangeField = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleCheckChangeOnClick = (event) => {
+    const allergyName = event.target.name;
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      setAllergyChoice([...allergyChoice, allergyName]);
+    }
+    else {
+      setAllergyChoice(allergyChoice.filter((allergy) => allergy !== allergyName));
+    }
+  };
+
+  const handleSubmitClick = (event) => {
+    event.preventDefault();
+    // Fonction d'envoi des données au BACKEND (POST ou UPDATE)
+  };
+
+  return (
+    <ProfileForm
+      email={email}
+      password={password}
+      name={name}
+      lastname={lastname}
+      adress={adress}
+      zipcode={zipcode}
+      city={city}
+      emailChangeField={handleEmailChangeField}
+      passwordChangeField={handlePasswordChangeField}
+      nameChangeField={handleNameChangeField}
+      lastnameChangeField={handleLastnameChangeField}
+      adressChangeField={handleAdressChangeField}
+      zipcodeChangeField={handleZipcodeChangeField}
+      cityChangeField={handleCityChangeField}
+      checkChangeOnClick={handleCheckChangeOnClick}
+      handleClick={handleSubmitClick}
+    />
+  );
 };
 
 export default Profil;
