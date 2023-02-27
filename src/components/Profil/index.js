3059280425ba95e7e7659dd65449e56f8ceba678
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-02-27 16:12:20 +0100

    :bug: Fix bug LoginForm

--- src/components/Profil/index.js
+++ src/components/Profil/index.js
@@ -1,15 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import './styles.scss';
 import ProfileForm from '../ProfileForm';
 
 const Profil = () => {
-  const handleSubmitClick = (event) => {
+
+  const navigate = useNavigate();
+
+   /* const handleSubmitClick = (event) => {
     event.preventDefault();
+    navigate('/');
+    console.log(event);
     // Fonction d'envoi des données au BACKEND (POST ou UPDATE)
-  };
+  }; */
 
   return (
     <ProfileForm
-      handleClick={handleSubmitClick}
+      // handleClick={handleSubmitClick}
     />
   );
 };
--- src/components/ProfileForm/index.js
+++ src/components/ProfileForm/index.js
@@ -54,6 +54,13 @@ const ProfileForm = ({ handleClick }) => {
     }
   };
 
+  const handleSubmitClick = (event) => {
+    event.preventDefault();
+    navigate('/');
+    console.log(event);
+    // Fonction d'envoi des données au BACKEND (POST ou UPDATE)
+  };
+
   return (
     <div>
       <form>
@@ -109,7 +116,7 @@ const ProfileForm = ({ handleClick }) => {
         <Allergy
           hundleClick={handleCheckChangeOnClick}
         />
-        <button onClick={handleClick} type="submit">Enregistrer</button>
+        <input onSubmit={handleSubmitClick} type="submit" value="Enregistrer" />
       </form>
     </div>
   );
