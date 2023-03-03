8283bb36e23dbfe96001c5369880177519489b3a
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-03-03 10:16:37 +0100

    :bug: Fix Bug [Router] page cgu

--- src/components/Cgu/index.js
+++ src/components/Cgu/index.js
@@ -1,7 +1,12 @@
+import Header from "../Header";
+import Footer from "../Footer";
+
 const Cgu = () => {
     return (
         <div>
+            <Header />
             <h1> Ceci est la page qui va montrer les conditions générales d'utilisation </h1>
+            <Footer />
         </div>
     )
 };
