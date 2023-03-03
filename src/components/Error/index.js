b581665ce15a0a6505b8c6242080331eb2a13309
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-03-03 10:04:17 +0100

    :bug: Fix Bug [Router] Error Page

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,9 +1,12 @@
+import Header from "../Header";
+import Footer from "../Footer";
+
 const Error = () => {
     return (
         <div>
-            
+            <Header />
             <h1> Une erreur s'est produite </h1>
-            
+            <Footer />
         </div>
     )
 };
