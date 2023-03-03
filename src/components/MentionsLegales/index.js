e5f1464c419bc464072990cb0f3c88c11abc76b0
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-03-03 10:08:02 +0100

    :bug: Fix bug [Router] Mentions Legales

--- src/components/MentionsLegales/index.js
+++ src/components/MentionsLegales/index.js
@@ -1,7 +1,12 @@
+import Header from "../Header";
+import Footer from "../Footer";
+
 const MentionsLegales = () => {
     return (
             <div>
+                <Header />
                 <p>Ce site n'équivaut pas à un avis médical</p>
+                <Footer />
             </div>
     )
     
