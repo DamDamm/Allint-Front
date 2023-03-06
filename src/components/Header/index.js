1d342464f5827dc20c3b62416b3ced845f6eb479
LordThi <92553090+LordThi@users.noreply.github.com>
2023-03-06 15:39:24 +0100

    :lipstick: feat(Header): add logo + CSS logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,25 @@
-import Profil from "./Profil";
-import Connexion from "./Connexion";
-import Deconnexion from "./Deconnexion";
+import { Link } from 'react-router-dom';
+import Profil from './Profil';
+import Connexion from './Connexion';
+import Deconnexion from './Deconnexion';
 
-import { Link } from "react-router-dom";
+import './styles.scss';
+import logo from '../../assets/img/logo-allint.png';
 
-import './styles.scss'
+// We get the LoginForm props and pass it in the header
+const Header = ({ isLogged, isDisconnected }) => (
+  <header className="header">
 
-//We get the LoginForm props and pass it in the header
-const Header = ({isLogged, isDisconnected}) => {
+    <Link to="/">
+      <img src={logo} className="header-img" />
+    </Link>
 
-    return (
-        <header className="header">
-            
-            <Link to="/">
-            <img src="#" className="header-img"/>
-            </Link>
-
-            <div className="header-btns">
-                <Profil isLogged={isLogged} /*We pass the props in Connexion element *//> 
-                <Connexion isLogged={isLogged} /*We pass the props in Connexion element */ /> 
-                <Deconnexion isLogged={isLogged} isDisconnected={isDisconnected} /*We pass the props in Connexion element *//> 
-            </div>
-        </header>
-    )
-};
+    <div className="header-btns">
+      <Profil isLogged={isLogged} /* We pass the props in Connexion element *//>
+      <Connexion isLogged={isLogged} /* We pass the props in Connexion element */ />
+      <Deconnexion isLogged={isLogged} isDisconnected={isDisconnected} /* We pass the props in Connexion element *//>
+    </div>
+  </header>
+);
 
 export default Header;
