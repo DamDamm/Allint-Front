2544ff7a6abe75d12e7e5fe9b5cbc9af5033d076
LordThi <92553090+LordThi@users.noreply.github.com>
2023-03-06 16:09:41 +0100

    :sparkles: feat(App): add waiting animation on homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,29 +1,39 @@
 import PropTypes from 'prop-types';
 import Page from '../Page';
 import Header from '../Header';
-import Search from "../Search";
-import Result from "../Results";
+import Search from '../Search';
+import Result from '../Results';
 
-//Destructuring props to use them
-const Home = ({product, setProduct, productResult, setProductResult, isLogged, isConnected, isDisconnected}) => {
-  return (
-    <Page>
-      <Header isLogged={isLogged} isConnected={isConnected} isDisconnected={isDisconnected}/>
-      <div>
-        <Search product={product}
-            setProduct={setProduct}
-            productResult={productResult}
-            setProductResult={setProductResult}/>
-        <Result productResult={productResult} />
-      </div>
-    </Page>
-  );
-};
-  
+import './styles.scss';
+import cart from '../../assets/img/cart.gif';
+
+// Destructuring props to use them
+const Home = ({
+  product, setProduct, productResult, setProductResult, isLogged, isConnected, isDisconnected,
+}) => (
+  <Page>
+    <Header isLogged={isLogged} isConnected={isConnected} isDisconnected={isDisconnected} />
+    <div>
+      <Search
+        product={product}
+        setProduct={setProduct}
+        productResult={productResult}
+        setProductResult={setProductResult}
+      />
+      {!product
+      && (
+      <div className="waiting_cart" />
+      )}
+      {product && <Result productResult={productResult} />}
+
+    </div>
+  </Page>
+);
+
 Home.propTypes = {
-  isLogged: PropTypes.bool, 
+  isLogged: PropTypes.bool,
   isConnected: PropTypes.func,
-  isDisconnected:PropTypes.func,
- };
+  isDisconnected: PropTypes.func,
+};
 
-  export default Home;
+export default Home;
