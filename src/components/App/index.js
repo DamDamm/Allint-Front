f148e9c4205e152599ad219d4543a36a45ede006
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-03-06 12:02:06 +0100

    :bug: Fix bug [Router] NousContacter

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,15 +7,15 @@ import Home from '../Home';
 import Login from '../Login';
 import Profil from '../Profil';
 import Product from 'src/components/Product';
-import Apropos from '../Apropos';
 import Footer from '../Footer';
+import Apropos from '../Apropos';
+import NousContacter from '../NousContacter';
 import MentionsLegales from '../MentionsLegales';
 import Cgu from '../Cgu';
 import Error from '../Error'; 
 
-
+//Router
 import { Routes, Route } from 'react-router-dom';
-import NousContacter from '../NousContacter';
 
 
 // == App
@@ -54,6 +54,7 @@ function App() {
           <Route exact path='/*' element={<Error isLogged={isLoggedInApp} isConnected={userConnected} isDisconnected={userDisconnected}/>}/>
           <Route exact path ='/mentionslegales' element={<MentionsLegales isLogged={isLoggedInApp} isConnected={userConnected} isDisconnected={userDisconnected}/>} />
           <Route exact path ='/apropos' element={<Apropos isLogged={isLoggedInApp} isConnected={userConnected} isDisconnected={userDisconnected}/>} />
+          <Route exact path ='/nouscontacter' element={<NousContacter isLogged={isLoggedInApp} isConnected={userConnected} isDisconnected={userDisconnected}/>} />
           <Route exact path ='/cgu' element={<Cgu isLogged={isLoggedInApp} isConnected={userConnected} isDisconnected={userDisconnected}/>} />
         </Routes>
         <Footer />
