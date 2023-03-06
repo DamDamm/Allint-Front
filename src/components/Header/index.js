2cbf3d9425d24ecd9f0468bb0cf1a0a315628f28
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-03-06 12:28:28 +0100

    :sparkles: Add link to Header/img for redirect to Home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,8 @@ import Profil from "./Profil";
 import Connexion from "./Connexion";
 import Deconnexion from "./Deconnexion";
 
+import { Link } from "react-router-dom";
+
 import './styles.scss'
 
 //We get the LoginForm props and pass it in the header
@@ -9,7 +11,11 @@ const Header = ({isLogged, isDisconnected}) => {
 
     return (
         <header className="header">
+            
+            <Link to="/">
             <img src="#" className="header-img"/>
+            </Link>
+
             <div className="header-btns">
                 <Profil isLogged={isLogged} /*We pass the props in Connexion element *//> 
                 <Connexion isLogged={isLogged} /*We pass the props in Connexion element */ /> 
