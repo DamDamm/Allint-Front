f1d006ac23b21034dd9d997220cc9c46cf270fa8
LordThi <92553090+LordThi@users.noreply.github.com>
2023-03-08 08:38:47 +0100

    :sparkles: feat (App): introducing Hooks -> axios useApi

---
+++ src/hooks/useApi.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://projet-08-allint-back-production.up.railway.app',
+});
+
+const useApi = () => {
+
+};
+
+export default useApi;
