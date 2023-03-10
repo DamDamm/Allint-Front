ef52e50d523473fa4f0761cb766800af388fc543
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-03-10 17:41:51 +0100

    :sparkles: Add cgu and mentions legales

--- src/components/Cgu/index.js
+++ src/components/Cgu/index.js
@@ -1,30 +1,22 @@
 import './styles.scss'
 
-const Cgu = () => {
-    return (
-        <div className="cgu">
-            <ul className="cards">
-            <li className='cards-card'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue placerat urna. Suspendisse rutrum massa vitae ligula euismod, ut sodales dui ultricies. Integer justo orci, luctus vel velit eu, fringilla lacinia felis. Praesent rhoncus sagittis venenatis. Ut id tortor id metus varius cursus. Quisque metus velit, fringilla sed lectus ac, bibendum lacinia nibh. Ut sed luctus tellus. Praesent vel fermentum elit. Sed nec finibus lectus. Nam sit amet augue quis tortor tristique vestibulum vel ac elit. Nam nunc erat, pharetra ac feugiat ac, dictum eu lectus. Nulla non nisi libero. Curabitur risus nisi, tempus id imperdiet nec, posuere a lectus. Morbi et mollis risus. Morbi ut elit pulvinar, placerat enim at, aliquam lacus.</li>
-
-            <li className='cards-card'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue placerat urna. Suspendisse rutrum massa vitae ligula euismod, ut sodales dui ultricies. Integer justo orci, luctus vel velit eu, fringilla lacinia felis. Praesent rhoncus sagittis venenatis. Ut id tortor id metus varius cursus. Quisque metus velit, fringilla sed lectus ac, bibendum lacinia nibh. Ut sed luctus tellus. Praesent vel fermentum elit. Sed nec finibus lectus. Nam sit amet augue quis tortor tristique vestibulum vel ac elit. Nam nunc erat, pharetra ac feugiat ac, dictum eu lectus. Nulla non nisi libero. Curabitur risus nisi, tempus id imperdiet nec, posuere a lectus. Morbi et mollis risus. Morbi ut elit pulvinar, placerat enim at, aliquam lacus.</li>
-
-            <li className='cards-card'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue placerat urna. Suspendisse rutrum massa vitae ligula euismod, ut sodales dui ultricies. Integer justo orci, luctus vel velit eu, fringilla lacinia felis. Praesent rhoncus sagittis venenatis. Ut id tortor id metus varius cursus. Quisque metus velit, fringilla sed lectus ac, bibendum lacinia nibh. Ut sed luctus tellus. Praesent vel fermentum elit. Sed nec finibus lectus. Nam sit amet augue quis tortor tristique vestibulum vel ac elit. Nam nunc erat, pharetra ac feugiat ac, dictum eu lectus. Nulla non nisi libero. Curabitur risus nisi, tempus id imperdiet nec, posuere a lectus. Morbi et mollis risus. Morbi ut elit pulvinar, placerat enim at, aliquam lacus.</li>
-
-            <li className='cards-card'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue placerat urna. Suspendisse rutrum massa vitae ligula euismod, ut sodales dui ultricies. Integer justo orci, luctus vel velit eu, fringilla lacinia felis. Praesent rhoncus sagittis venenatis. Ut id tortor id metus varius cursus. Quisque metus velit, fringilla sed lectus ac, bibendum lacinia nibh. Ut sed luctus tellus. Praesent vel fermentum elit. Sed nec finibus lectus. Nam sit amet augue quis tortor tristique vestibulum vel ac elit. Nam nunc erat, pharetra ac feugiat ac, dictum eu lectus. Nulla non nisi libero. Curabitur risus nisi, tempus id imperdiet nec, posuere a lectus. Morbi et mollis risus. Morbi ut elit pulvinar, placerat enim at, aliquam lacus.</li>
-
-            <li className='cards-card'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue placerat urna. Suspendisse rutrum massa vitae ligula euismod, ut sodales dui ultricies. Integer justo orci, luctus vel velit eu, fringilla lacinia felis. Praesent rhoncus sagittis venenatis. Ut id tortor id metus varius cursus. Quisque metus velit, fringilla sed lectus ac, bibendum lacinia nibh. Ut sed luctus tellus. Praesent vel fermentum elit. Sed nec finibus lectus. Nam sit amet augue quis tortor tristique vestibulum vel ac elit. Nam nunc erat, pharetra ac feugiat ac, dictum eu lectus. Nulla non nisi libero. Curabitur risus nisi, tempus id imperdiet nec, posuere a lectus. Morbi et mollis risus. Morbi ut elit pulvinar, placerat enim at, aliquam lacus.</li>
-
-            <li className='cards-card'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue placerat urna. Suspendisse rutrum massa vitae ligula euismod, ut sodales dui ultricies. Integer justo orci, luctus vel velit eu, fringilla lacinia felis. Praesent rhoncus sagittis venenatis. Ut id tortor id metus varius cursus. Quisque metus velit, fringilla sed lectus ac, bibendum lacinia nibh. Ut sed luctus tellus. Praesent vel fermentum elit. Sed nec finibus lectus. Nam sit amet augue quis tortor tristique vestibulum vel ac elit. Nam nunc erat, pharetra ac feugiat ac, dictum eu lectus. Nulla non nisi libero. Curabitur risus nisi, tempus id imperdiet nec, posuere a lectus. Morbi et mollis risus. Morbi ut elit pulvinar, placerat enim at, aliquam lacus.</li>
-
-            </ul>
-        </div>
-    )
-};
+    const Cgu = () => {
+        return (
+    <div className="cgu">
+        <ul className='Cards'>
+            <li>L'utilisateur reconnaît que les informations fournies sur ce site web ne remplacent pas l'avis médical professionnel et ne doivent pas être utilisées pour diagnostiquer ou traiter une allergie alimentaire.</li>
+            <li>Le contenu de ce site web est fourni à titre informatif seulement et ne garantit pas l'exactitude, l'exhaustivité ou la pertinence des informations.</li>
+            <li>L'utilisateur reconnaît que l'utilisation des informations contenues sur ce site web se fait à ses propres risques et périls. Le propriétaire du site décline toute responsabilité quant à l'utilisation des informations contenues sur ce site.</li>
+            <li>L'utilisateur ne doit pas utiliser ce site web à des fins illicites ou pour violer les droits d'autrui.</li>
+            <li>Le propriétaire du site web se réserve le droit de modifier, suspendre ou mettre fin à tout moment et sans préavis les services et fonctionnalités offerts sur ce site web.</li>
+            <li>L'utilisateur est responsable de maintenir la confidentialité de son compte et de son mot de passe, ainsi que de toutes les activités qui se produisent sous son compte.</li>
+            <li>L'utilisateur accepte de ne pas copier, reproduire, distribuer ou exploiter de quelque manière que ce soit le contenu de ce site web sans l'autorisation expresse du propriétaire du site.</li>
+            <li>Le propriétaire du site ne garantit pas que le site web sera exempt d'erreurs, de virus ou d'autres éléments nuisibles.</li>
+            <li>L'utilisateur accepte de ne pas utiliser ce site web pour collecter des informations personnelles sur d'autres utilisateurs ou pour envoyer du spam ou des communications non sollicitées.</li>
+            <li>En utilisant ce site web, l'utilisateur accepte toutes les conditions générales d'utilisation énoncées ci-dessus. Si l'utilisateur n'accepte pas ces conditions, il doit immédiatement cesser d'utiliser le site.</li>
+        </ul>
+    </div>
+        )
+    };
 
 export default Cgu;
